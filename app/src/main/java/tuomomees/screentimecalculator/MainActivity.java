f9d6bbd7b5e96d4b1833d84fa17d38a5fea09dc8
tuomomees <tuomo95@gmail.com>
2017-09-24 16:39:23 +0300

    Lukuisia ulkoasumuutoksia. Uusi thread, joka hoitaa työt optimoidummalla koodilla (syrjäyttää vanhan). Kaksi uutta fragmentia, jotka on suunniteltu ja optimoitu paremmin (syrjäyttävät kaksi vanhaa).

--- app/src/main/java/tuomomees/screentimecalculator/MainActivity.java
+++ app/src/main/java/tuomomees/screentimecalculator/MainActivity.java
@@ -37,6 +37,7 @@ public class MainActivity extends FragmentActivity {
     MostUsedAppsFragment mostUsedAppsFragment;
     LastUsedAppsFragment lastUsedAppsFragment;
 
+    //asd
 
     Thread appStatsQueryThread;
     Thread appStatsWeeklyQueryThread;
